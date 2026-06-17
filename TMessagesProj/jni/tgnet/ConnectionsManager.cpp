12e5f76aced03356a3ef7910310853b1690e7079
test2793 <test@test.com>
2026-06-17 14:00:20 +0300

    Change server address

--- TMessagesProj/jni/tgnet/ConnectionsManager.cpp
+++ TMessagesProj/jni/tgnet/ConnectionsManager.cpp
@@ -1817,7 +1817,7 @@ void ConnectionsManager::initDatacenters() {
     if (!testBackend) {
         if (datacenters.find(1) == datacenters.end()) {
             datacenter = new Datacenter(instanceNum, 1);
-            datacenter->addAddressAndPort("43.155.11.190", 10443, 0, "");
+            datacenter->addAddressAndPort("89.22.236.201", 10443, 0, "");
             // datacenter->addAddressAndPort("149.154.175.50", 443, 0, "");
             // datacenter->addAddressAndPort("2001:b28:f23d:f001:0000:0000:0000:000a", 443, 1, "");
             datacenters[1] = datacenter;
@@ -1856,7 +1856,7 @@ void ConnectionsManager::initDatacenters() {
     } else {
         if (datacenters.find(1) == datacenters.end()) {
             datacenter = new Datacenter(instanceNum, 1);
-            datacenter->addAddressAndPort("43.155.11.190", 10443, 0, "");
+            datacenter->addAddressAndPort("89.22.236.201", 10443, 0, "");
             // datacenter->addAddressAndPort("149.154.175.40", 443, 0, "");
             // datacenter->addAddressAndPort("2001:b28:f23d:f001:0000:0000:0000:000e", 443, 1, "");
             datacenters[1] = datacenter;
